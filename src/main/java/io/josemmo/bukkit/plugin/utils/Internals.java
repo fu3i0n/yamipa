e108cff5f6ebba5897ef0565294b78538fe3e756
Daisy <amari@amari.wtf>
2025-04-16 23:55:20 +0100

    Refactor to use PacketEvents for packet handling and update ActionBar timing

--- src/main/java/io/josemmo/bukkit/plugin/utils/Internals.java
+++ src/main/java/io/josemmo/bukkit/plugin/utils/Internals.java
@@ -1,19 +1,21 @@
 package io.josemmo.bukkit.plugin.utils;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
+import com.github.retrooper.packetevents.PacketEvents;
+import com.github.retrooper.packetevents.manager.server.ServerVersion;
+import com.github.retrooper.packetevents.protocol.ProtocolVersion;
+import com.github.retrooper.packetevents.util.PEVersion;
+import com.mojang.brigadier.CommandDispatcher;
 import org.bukkit.Bukkit;
 import org.bukkit.Server;
 import org.bukkit.command.CommandMap;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import com.comphenix.protocol.reflect.FuzzyReflection;
-import com.comphenix.protocol.utility.MinecraftReflection;
-import com.mojang.brigadier.CommandDispatcher;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class Internals {
-    public static final float MINECRAFT_VERSION;
+    public static final ServerVersion MINECRAFT_VERSION;
     private static final CommandDispatcher<?> DISPATCHER;
     private static final CommandMap COMMAND_MAP;
     private static @Nullable Method GET_BUKKIT_SENDER_METHOD = null;
@@ -21,31 +23,35 @@ public class Internals {
     static {
         try {
             // Get Minecraft version
-            String version = Bukkit.getVersion();
-            version = version.substring(version.lastIndexOf("(MC: 1.")+7, version.length()-1);
-            MINECRAFT_VERSION = Float.parseFloat(version);
+            MINECRAFT_VERSION = PacketEvents.getAPI().getServerManager().getVersion();
 
             // Get "org.bukkit.craftbukkit.CraftServer" references
             Server obcInstance = Bukkit.getServer();
             Class<?> obcClass = obcInstance.getClass();
 
             // Get "net.minecraft.server.MinecraftServer" references
-            Object nmsServerInstance = obcClass.getDeclaredMethod("getServer").invoke(obcInstance);
+            Object nmsInstance = obcClass.getDeclaredMethod("getServer").invoke(obcInstance);
+            Class<?> nmsClass = nmsInstance.getClass().getSuperclass();
 
-            // Get "net.minecraft.server.CommandDispatcher" references
-            Class<?> nmsDispatcherClass = MinecraftReflection.getMinecraftClass(
-            	"CommandDispatcher", // Spigot <1.17
-            	"commands.CommandDispatcher", // Spigot >=1.17
-            	"commands.Commands" // PaperMC
-            );
-            Object nmsDispatcherInstance = FuzzyReflection.fromObject(nmsServerInstance, true)
-            	.getMethodByReturnTypeAndParameters("getDispatcher", nmsDispatcherClass)
-            	.invoke(nmsServerInstance);
+            if (MINECRAFT_VERSION.isNewerThanOrEquals(ServerVersion.V_1_20_5)) {
+                // Get "net.minecraft.commands.Commands" references
+                Object nmsCommandsInstance = nmsClass.getDeclaredMethod("getCommands").invoke(nmsInstance);
+                Class<?> nmsCommandsClass = nmsCommandsInstance.getClass();
 
-            // Get "com.mojang.brigadier.CommandDispatcher" instance
-            DISPATCHER = (CommandDispatcher<?>) FuzzyReflection.fromObject(nmsDispatcherInstance, true)
-            	.getMethodByReturnTypeAndParameters("getDispatcher", CommandDispatcher.class)
-            	.invoke(nmsDispatcherInstance);
+                // Get "com.mojang.brigadier.CommandDispatcher" instance
+                Field nmsDispatcherField = nmsCommandsClass.getDeclaredField("dispatcher");
+                nmsDispatcherField.setAccessible(true);
+                DISPATCHER = (CommandDispatcher<?>) nmsDispatcherField.get(nmsCommandsInstance);
+            } else {
+                // Get "net.minecraft.server.CommandDispatcher" references
+                Object nmsDispatcherInstance = nmsClass.getDeclaredField("vanillaCommandDispatcher").get(nmsInstance);
+                Class<?> nmsDispatcherClass = nmsDispatcherInstance.getClass();
+
+                // Get "com.mojang.brigadier.CommandDispatcher" instance
+                Method getDispatcherMethod = nmsDispatcherClass.getDeclaredMethod("a");
+                getDispatcherMethod.setAccessible(true);
+                DISPATCHER = (CommandDispatcher<?>) getDispatcherMethod.invoke(nmsDispatcherInstance);
+            }
 
             // Get command map instance
             Field commandMapField = obcClass.getDeclaredField("commandMap");
